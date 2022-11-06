0fa7a4879e38c3b9b58b7ec6bb7ba726a06d0fe5
Bas Man <bas.man.developer@gmail.com>
2022-11-06 17:35:07 +0000

    res test count letters

--- spec/reading_spec.rb
+++ spec/reading_spec.rb
@@ -3,12 +3,17 @@ require_relative '../lib/reading'
 describe 'reads user input' do
   it 'can read a string entered' do
     user_string = TextInput.new('The rain in Spain falls mainly on the plane.')
-
     expect(user_string.name).to eq('The rain in Spain falls mainly on the plane.')
   end
 
   it 'can count how many words in a sentence' do
     user_string = TextInput.new('The rain in Spain falls mainly on the plane.')
     expect(user_string.count_words).to eq(9)
+  end
+
+  it 'can how many letters in the sentence' do
+    user_string = TextInput.new('The rain in Spain falls mainly on the plane.')
+    expect(user_string.count_letters).to eq(32)
+
   end
 end
